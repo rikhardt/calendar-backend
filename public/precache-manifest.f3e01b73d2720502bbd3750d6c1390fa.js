2e26d4b65078fe7b6a08c94f334502897089a537
ricardosotosaez <ricardosoto.saez@gmail.com>
2020-10-16 16:25:32 -0300

    v1 calendar app

---
+++ public/precache-manifest.f3e01b73d2720502bbd3750d6c1390fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbcfa316d553586c14e2741d25aad12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c420f71953c20d6bfd85",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "996bc56b7ec9f61d4022",
+    "url": "/static/css/main.8f0ed365.chunk.css"
+  },
+  {
+    "revision": "c420f71953c20d6bfd85",
+    "url": "/static/js/2.1ae2c356.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.1ae2c356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996bc56b7ec9f61d4022",
+    "url": "/static/js/main.cce80026.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
